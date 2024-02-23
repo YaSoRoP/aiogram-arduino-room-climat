4356d19e505e1139102195249d23fdc2d77cc205
Yaroslav <103318612+YaSoRoP@users.noreply.github.com>
2024-02-23 11:51:07 +0300

    Вывод логов для стартового сообщения

--- aiogram_arduino_room_climat/telegram_bot/handlers/handler.py
+++ aiogram_arduino_room_climat/telegram_bot/handlers/handler.py
@@ -2,11 +2,15 @@ from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import CommandStart
 from lexicon.lexicon_ru import LEXICON_RU
-
+from utils.logger import logger
 
 router = Router()
 
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
+    logger.info(LEXICON_RU['log_info_cmd_start'].format(
+        message.from_user.full_name, 
+        message.from_user.id))
+    
     await message.answer(LEXICON_RU['cmd_start'].format(message.from_user.full_name))
