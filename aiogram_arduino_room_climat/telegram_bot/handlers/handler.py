c22184ca988cd7ce0c23e392238f43825cc91370
Yaroslav <103318612+YaSoRoP@users.noreply.github.com>
2024-02-23 11:46:34 +0300

    Вывод стартового сообщения
    - простейший вывод стартового сообщения

--- aiogram_arduino_room_climat/telegram_bot/handlers/handler.py
+++ aiogram_arduino_room_climat/telegram_bot/handlers/handler.py
@@ -1,6 +1,7 @@
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import CommandStart
+from lexicon.lexicon_ru import LEXICON_RU
 
 
 router = Router()
@@ -8,4 +9,4 @@ router = Router()
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer('Привет, работяга')
+    await message.answer(LEXICON_RU['cmd_start'].format(message.from_user.full_name))
