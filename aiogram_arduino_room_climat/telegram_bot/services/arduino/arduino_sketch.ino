9aa598f801974528e55acb66bcff2a3c0ebcddf1
Yaroslav <103318612+YaSoRoP@users.noreply.github.com>
2024-02-23 13:38:21 +0300

    Create arduino_sketch.ino
    - Создание скетча для вывода информации с модуля DHT11 в COM-порт

---
+++ aiogram_arduino_room_climat/telegram_bot/services/arduino/arduino_sketch.ino
@@ -0,0 +1,39 @@
+#include <iarduino_DHT.h>
+
+iarduino_DHT sensor(2);
+
+void setup() {
+  Serial.begin(9600);
+  delay(1000);
+}
+
+void send_sensor_data() {
+  switch (sensor.read()) {
+    case DHT_OK:
+      Serial.println((String)sensor.hum + "% - " + sensor.tem + "°C");
+      break;
+    case DHT_ERROR_CHECKSUM:
+      Serial.println("HE PABEHCTBO KC");
+      break;
+    case DHT_ERROR_DATA:
+      Serial.println("OTBET HE COOTBETCTB. CEHCOPAM 'DHT'");
+      break;
+    case DHT_ERROR_NO_REPLY:
+      Serial.println("HET OTBETA");
+      break;
+    default:
+      Serial.println("ERROR");
+      break;
+  }
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String input = Serial.readString();
+    input.trim();
+    if (input == "request_information") {
+      send_sensor_data();
+    }
+  }
+  delay(2000);
+}
