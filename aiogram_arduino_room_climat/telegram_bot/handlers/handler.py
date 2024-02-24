9d322d8435a6229a9b73601498a38da774bac135
Yaroslav <103318612+YaSoRoP@users.noreply.github.com>
2024-02-24 15:00:03 +0300

    Update handler.py
    - Добавил комментарии к коду

--- aiogram_arduino_room_climat/telegram_bot/handlers/handler.py
+++ aiogram_arduino_room_climat/telegram_bot/handlers/handler.py
@@ -12,11 +12,21 @@ router = Router()
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
+    """
+    Обрабатывает команду /start.
+
+    Args:
+        message (Message): Объект сообщения.
+
+    Returns:
+        None
+    """
     logger.info(LEXICON_RU['log_info_cmd_start'].format(
         message.from_user.full_name, 
         message.from_user.id))
     
-    await message.delete()
+    await message.delete()  # Удаляем команду /start из чата
+    # Отправляем приветственное сообщение с инлайн-клавиатурой
     await message.answer(
         text=LEXICON_RU['cmd_start'].format(message.from_user.full_name),
         reply_markup=INLINE_KEYBOARD_REQUEST_INFORMATION)
@@ -24,13 +34,23 @@ async def cmd_start(message: Message):
 
 @router.callback_query(F.data.in_('request_information'))
 async def send_information(callback: CallbackQuery):
+    """
+    Обрабатывает запрос на получение информации.
+
+    Args:
+        callback (CallbackQuery): Объект callback-запроса.
+
+    Returns:
+        None
+    """
     logger.info(LEXICON_RU['log_info_callback_send_information'].format(
         callback.message.from_user.full_name, 
         callback.message.from_user.id))
+    # Редактируем текст сообщения с текущей датой-временем и данными с датчика
     await callback.message.edit_text(
         text=LEXICON_RU['callback_send_information'].format(
-            datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-            request_sensor_data()),
+            datetime.now().strftime("%Y-%m-%d %H:%M:%S"),  # Текущая дата-время
+            request_sensor_data()),  # Данные с датчика
         reply_markup=INLINE_KEYBOARD_REQUEST_INFORMATION
     )
-    await callback.answer()
+    await callback.answer()  # Отвечаем на callback-запрос
